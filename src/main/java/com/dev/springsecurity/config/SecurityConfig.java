5ded0111d41de3e96ef4d6901de4c10f030f1ac7
Mayara <mayaradepaula1997>
2025-03-23 16:32:11 -0300

    Update of the SecurityConfig configuration class (encode, decode and encryption)

--- src/main/java/com/dev/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/dev/springsecurity/config/SecurityConfig.java
@@ -1,23 +1,51 @@
 package com.dev.springsecurity.config;
 
+
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 
 @Configuration
 @EnableWebSecurity //aplica as regras de segurança das rotas de requisição
 public class SecurityConfig {
 
+    //Utilização da chave public e privada, inejeção das chave que vem atraves do "
+    // properties"
+
+    @Value("${jwt.public.key}")
+    private RSAPublicKey publicKey;
+
+    @Value("${jwt.private.key}")
+    private RSAPrivateKey privateKey;
+
+
+
 
     @Bean  //Configuração do Spring Security(rotas de requisição)
-    private SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
 
         http
-                .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST,"/login").permitAll()
+                        .anyRequest().authenticated())
                 .csrf(csrf -> csrf.disable())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
@@ -25,6 +53,33 @@ public class SecurityConfig {
         return http.build();
 
 
-
     }
+
+    //Esses métodos configuram como a aplicação irá decodificar e codificar os tokens JWT
+
+    @Bean
+    public JwtDecoder jwtDecoder(){
+
+        return NimbusJwtDecoder.withPublicKey(publicKey).build();
+    }
+
+
+    //Usado para criar tokens JWT assinados com a chave privada.
+
+    @Bean
+    public JwtEncoder jwtEncoder() {
+        JWK jwk = new RSAKey.Builder(this.publicKey).privateKey(privateKey).build();
+        var jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwks);
+    }
+
+    //Método que vai fazer a criptografia das senhas
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder (){
+
+        return new BCryptPasswordEncoder();
+    }
+
+
 }
