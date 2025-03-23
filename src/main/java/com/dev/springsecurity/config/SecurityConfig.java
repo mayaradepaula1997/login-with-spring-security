6317151d16a709cf397c483f4833472cab412e34
Mayara <mayaradepaula1997>
2025-03-23 10:38:00 -0300

    Spring Security configuration

---
+++ src/main/java/com/dev/springsecurity/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.dev.springsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity //aplica as regras de segurança das rotas de requisição
+public class SecurityConfig {
+
+
+    @Bean  //Configuração do Spring Security(rotas de requisição)
+    private SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+
+        http
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+                .csrf(csrf -> csrf.disable())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+
+
+
+    }
+}
