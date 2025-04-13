b4410ccee3056757bd95f7f39fc6dcf07bb29ab8
Mayara <mayaradepaula1997>
2025-04-13 11:28:55 -0300

    Dtos: FeedDto and FeedItemDto

---
+++ src/main/java/com/dev/springsecurity/controller/dto/FeedDto.java
@@ -0,0 +1,11 @@
+package com.dev.springsecurity.controller.dto;
+
+import java.util.List;
+
+public record FeedDto(List<FeedItemDto> feedItens,
+                      int page,
+                      int pageSize,
+                      int totalPages,
+                      long totalElements) {
+}
+
---
+++ src/main/java/com/dev/springsecurity/controller/dto/FeedItemDto.java
@@ -0,0 +1,4 @@
+package com.dev.springsecurity.controller.dto;
+
+public record FeedItemDto(long tweetId, String content, String username) {
+}
